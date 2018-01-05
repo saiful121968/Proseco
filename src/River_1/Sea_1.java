96324887b501f90a8938bd6904f8b7a2a87ba347
saiful <saiful121968@gmail.com>
2018-01-05 14:52:35 -0500

    Sea_1 added

---
+++ src/River_1/Sea_1.java
@@ -0,0 +1,16 @@
+package River_1;
+
+public class Sea_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5, b=10;
+		int x = a*a*a + 3*a*a*b + 3*a*b*b + b*b*b ;  
+		System.out.println("a = " +a + "\t b= " +b);
+		System.out.println("a*a*a + 3*a*a*b + 3*a*b*b + b*b*b= " +x);
+
+
+	}
+
+}
